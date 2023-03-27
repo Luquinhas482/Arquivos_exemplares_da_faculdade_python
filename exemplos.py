edc7b1ac382e966d158cecc1718ba3708471f35e
Luquinhas482 <99371201+Luquinhas482@users.noreply.github.com>
2023-03-27 10:18:40 -0300

    Add files via upload

---
+++ exemplos.py
@@ -0,0 +1,189 @@
+#Neste arquivo, eu irei colar alguns exemplos do professor fabricio de programação para serem de grande utilidade quando eu for realizar suas atividades
+
+print('========================aula 1.0:ola mundo===========================')
+
+#esta primeira linha de strings pertence ao primeiro exemplo dado em sala de aula e longe o mais simples tambem
+print('')
+
+print('Olá mundo!')
+print('')
+
+print('========================aula 1.1:gabriel===========================')
+print('')
+
+print('Informações do gabriel')
+print('')
+
+nome = "Gabriel"
+
+idade = 33
+
+altura = 1.75
+
+print('O nome do individuo é: ',nome)
+print('')
+
+print('A idade é: ',idade)    
+print('')                     #quando o print não é utilizado para imprimir um texto entre aspas,
+                              #mas em si é colocado o identificador de uma variavel, o print ira imprimir 
+print('a altura é: ',altura)  #os valores atribuidos a essa variavel.
+print('')
+
+
+print('========================aula 1.2:soma de alguns numericos===========================')
+
+num1 = 5    #o numero 5 foi atribuido a variavel um1, logo 
+            #tudo que utilizara o num1, ira ter como base o numero 5.
+
+
+num2 = 10   #o numero 10 foi atribuido a variavel um1, logo
+            #tudo que utilizara o num1, ira ter como base o numero 10.
+
+
+print ('num1 = 5 e num2 = 10')
+print('')
+
+print ('num1 + num2')
+print('')
+
+resultado = num1 + num2 #é necessario criar uma variavel que receba 
+                        #as outras variaveis anteriores contendo suas bases armazenadas 
+                        #e em seguida de um sinal aritmetico para realização do calculo.
+
+
+print(resultado)
+print('')
+
+
+print('========================aula 1.3: alguns exemplares de aritimeticos===========================')
+print('')
+
+#slide 21
+x = 14
+y = 62
+
+print('A questão se trata dos valores: x = 14 e y = 62')
+print('')
+
+#soma
+soma = x + y
+print(soma,' |Este valor pertence a soma')
+print('')
+
+#subtração
+subtracao = x - y
+print(subtracao,' |Este valor pertence a subtração')
+print('')
+
+#multiplicação
+multiplicacao = x * y
+print(multiplicacao,' |Este valor pertence a multiplicação')
+print('')
+
+#divisão
+divisao = x / y
+print(divisao,' |Este valor pertence a divisão')
+print('')
+
+#modulo
+modulo = x % y
+print(modulo,' |Este valor pertence ao resto da divisão')
+print('')
+
+#divisão inteira
+div_int = x // y
+print(div_int,' |Este valor pertence a divisão inteira')
+print('')
+
+#exponenciação
+exponenciacao = x ** y
+print(exponenciacao,' |Este valor pertence a exponenciação')
+print('')
+
+print('========================aula 1.4: Strings sendo somadas===========================')
+print('')
+
+#slide 29
+
+print ('A questão possui os valores: 25 + 60')
+print('')
+
+resultado0 = 25 + 60
+print(resultado0,' |valor somatorio normal')
+print('')
+
+resultado1 = "25" + "60"
+print(resultado1,' |Os valores estão entre aspas como um texto')
+print('')
+
+resultado2 = "Fabricio" + "Araujo"
+print(resultado2,' |Dois textos se somando')
+print('')
+
+print('Strings se somam diferentemente se fosse numeros')
+print('')
+
+print('========================aula 1.4: Fabricio com erro de resultado===========================')
+print('')
+
+
+#slide 29
+resultado3 = 25 + 60
+print(resultado3,' |Valores somados')
+print('')
+
+resultado4 = "25" + "60"
+print(resultado4,' |Valores como string')
+print('')
+
+resultado5 = "Fabricio" + "Araujo"
+print(resultado5,' |Strings sendo somadas')
+print('')
+
+print ('"Fabricio" + 37 : isso ira da um erro pois strings e numeros não se somam!')
+print('')
+
+print('========================aula 1.5: fabricio + str===========================')
+print('')
+
+print('No problema anterior, o fabricio não pode ser somado a um numero porem, o numero agora foi')
+print('transformado em uma string utilizando o str')
+print('')
+
+#slide 31
+print('valores = 25 + 60, "25" e "60".')
+resultado6 = 25 + 60
+print(resultado6,' |Soma')
+print('')
+
+
+resultado7 = "25" + "60"
+print('')
+
+print(resultado7,' |Strings somando')
+print('')
+
+
+resultado8 = "Fabricio" + "Araujo"
+print(resultado8,' |Textos somando')
+print('')
+
+print('Questão: "Fabricio" + str(37)')
+print('')
+
+resultado9 = "Fabricio" + str(37)
+print(resultado9,' |Com numero transformando, Fabricio pode ser somado.')
+print('')
+print('')
+print('')
+
+
+print('========================FINALIZAÇÃO DO PROGRAMA===========================')
+print('')
+
+print('OBRIGADO PELA SUA ATENÇÃO!')
+print('')
+print('')
+print('')
+
+
